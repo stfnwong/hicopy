ab5367f8b1f32bdea1db16f9e5bdff783c4345e9
Stefan Wong <stfnwong@gmail.com>
2024-05-19 11:17:11 +0100

    Exceptions everywhere

--- example.py
+++ example.py
@@ -14,6 +14,9 @@ example_inputs = [
     ["atom", ["quote", [1, 2, 3]]],
     ["eq", 1, 1],
     ["eq", 1, 2],
+    ["car", ["quote", [1000, 20]]],
+    ["cdr", ["quote", [1000, 20, 1.1]]],
+    ["car", ["car", ["cdr", ["cdr", ["cdr", ["quote", [1, 2, 10, [50, 100]]]]]]]],
 ]
 
 
@@ -23,7 +26,7 @@ def main(examples: List[Any]) -> None:
         try:
             print(f"{n}: [{ex}] -> {leval(ex)}")
         except Exception as e:
-            print(f"{n}: [{ex}], {e}")
+            print(f"{n}: EXCEPTION [{ex}], {e}")
 
 
 if __name__ == "__main__":
