762eb9b27d8c1944c3fcd6b47ffa959b27c67ba7
Stefan Wong <stfnwong@gmail.com>
2024-05-19 11:04:20 +0100

    Some unit tests.
    I think a lot of these tests are missing the point, rather than use a
    testing framework for examples I should just write examples directly.

--- hicopy/__init__.py
+++ hicopy/__init__.py
@@ -1,1 +1,32 @@
 # Maybe just put everything here?
+
+#from typing import List
+
+atom = lambda x: not isinstance(x, list)
+eq = lambda x, y: x == y
+
+car = lambda x: x[0]
+cdr = lambda x: x[1:]
+
+cons = lambda x, y: [x, y] if atom(y) else [x] + y
+
+
+#from pudb import set_trace; set_trace()
+# Lisp eval - dumb name to avoid clash with python eval()
+def leval(x):
+    if atom(x):                 return x
+    elif eq(car(x), "quote"):   return car(cdr(x))
+    elif eq(car(x), "atom"):    return atom(eval(car(cdr(x))))
+    elif eq(car(x), "eq"):      return eq(
+        eval(car(cdr(x))),
+        eval(car(cdr(cdr(x))))
+    )
+    elif eq(car(x), "car"):     return car(eval(car(cdr(x))))
+    elif eq(car(x), "cdr"):     return cdr(eval(car(cdr(x))))
+    elif eq(car(x), "cons"):    return cons(eval(car(cdr(x))), eval(car(cdr(cdr(x)))))
+    elif eq(car(x), "cond"):
+        for i in cdr(x):
+            if eval(car(i)):
+                return eval(car(cdr(i)))
+
+    return []
--- test/test_hicopy.py
+++ test/test_hicopy.py
@@ -1,1 +1,68 @@
 # All the tests should fit in a single file
+
+from hicopy import (
+    atom, 
+    car,
+    cdr,
+    leval
+)
+
+
+# Tests for basic stuff - honestly isn't life a bit short to write all these?
+def test_atomic_ops():
+    inp = ["atom", ["quote", [1, 2, 3]]]
+
+    #from pudb import set_trace; set_trace()
+    assert car(inp) == "atom"
+    assert cdr(inp) == [["quote", [1, 2, 3]]]    # TODO: legit?
+    assert car(cdr(inp)) == ["quote", [1, 2, 3]]
+    #assert car(cdr(cdr(inp))) == [1, 2, 3]
+
+
+
+
+def test_car_cdr():
+    inp = ["atom", [1, 2, 3], [4, 5, 6]]
+
+    assert car(inp) == "atom"
+
+
+#def test_eval_atom():
+#    ret = leval([atom])
+#
+#    assert isinstance(ret, list)
+#    assert len(ret) == 0
+#
+#    ret = leval(["atom", 1])
+#    print(ret)
+#
+
+def test_eval_const():
+    assert isinstance(leval(42), int)
+    assert isinstance(leval(42.0), float)
+
+
+def test_quote():
+    # quote bypasses the evaluation rule and passes the data along exactly
+    pass
+
+
+def test_eq():
+    inp = ["eq", 1, 1]
+    assert car(inp) == "eq"
+    assert cdr(inp) == [1, 1]
+    assert car(cdr(inp)) == 1
+    assert car(cdr(cdr(inp))) == 1
+
+
+
+def test_eval_atom():
+    inp = ["atom", 1]
+
+    try:
+        print(leval(inp))
+    except Exception as e:
+        print(e)
+
+    assert leval(inp)       # Returns True
+
