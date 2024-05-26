8ac7096083f85a8baad91330e7aa5b7cd571cc0a
Stefan Wong <stfnwong@gmail.com>
2024-05-26 15:41:31 +0100

    Some test cleanup

--- test/test_hicopy.py
+++ test/test_hicopy.py
@@ -20,8 +20,6 @@ def test_atomic_ops():
     assert car(cdr(inp)) == ["quote", [1, 2, 3]]
 
 
-
-
 def test_car_cdr():
     inp = ["atom", [1, 2, 3], [4, 5, 6]]
 
@@ -33,18 +31,12 @@ def test_eval_const():
     assert isinstance(leval(42.0), float)
 
 
-def test_quote():
-    # quote bypasses the evaluation rule and passes the data along exactly
-    pass
-
-
 def test_eq():
     inp = ["eq", 1, 1]
     assert car(inp) == "eq"
     assert cdr(inp) == [1, 1]
     assert car(cdr(inp)) == 1
     assert car(cdr(cdr(inp))) == 1
-
 
 
 def test_eval_atom():
@@ -56,8 +48,6 @@ def test_eval_atom():
         print(e)
 
     assert leval(inp)       # Returns True
-
-
 
 
 def test_eval():
@@ -78,12 +68,6 @@ def test_eval():
         ["cond", [["eq", 1, 1], "true"], [["atom", ["quote", 2]], "false"], ["t", "default"]],
         ["cond", [["atom", ["quote", 2]], "cake"], ["t", "default"]],
         ["cond", [["atom", ["quote", [1, 2]]], "false"], ["t", "default"]],
-        # assoc
-        assoc("a", [["a", 1], ["b", 2], ["c", 3]]),   # ["a", 1]
-        assoc("d", [["a", 1], ["b", 2], ["c", 3]]),   # []
-        # pairlis
-        pairlis(["x", "y"], [1, 2]),     # zip, so [[x, 1], [y, 2]]
-        pairlis(["x", "y"], [1, 2, 5]),  # zip, so [[x, 1], [y, 2]]
     ]
 
     exp_outputs = [
@@ -103,12 +87,6 @@ def test_eval():
         "true",
         "cake",
         "default",
-        # assoc
-        ["a", 1],
-        [],
-        # pairlis 
-        [["x", 1], ["y", 2]],
-        [["x", 1], ["y", 2]],
     ]
 
     # Check we didn't fuck up the spec
@@ -118,13 +96,7 @@ def test_eval():
         assert leval(inp) == exp_out
 
 
-
 def test_assoc():
-    inputs = [
-        assoc("a", [["a", 1], ["b", 2], ["c", 3]]),   # ["a", 1]
-        assoc("d", [["a", 1], ["b", 2], ["c", 3]]),   # []
-    ]
-
     # Format is (target, input)
     inputs = [
         ("a", [["a", 1], ["b", 2], ["c", 3]]),
@@ -136,7 +108,21 @@ def test_assoc():
         [],
     ]
 
-
-    for n, (inp, exp_out) in enumerate(zip(inputs, exp_outputs)):
-        print(n, leval(inp))
+    for inp, exp_out in zip(inputs, exp_outputs):
         assert assoc(inp[0], inp[1]) == exp_out
+
+
+def test_pairlis():
+    # Format is (x, y)
+    inputs = [
+        (["x", "y"], [1, 2]),
+        (["x", "y"], [1, 2, 5]),
+    ]
+
+    exp_outputs = [
+        [["x", 1], ["y", 2]],
+        [["x", 1], ["y", 2]],
+    ]
+
+    for inp, exp_out in zip(inputs, exp_outputs):
+        assert pairlis(inp[0], inp[1]) == exp_out
